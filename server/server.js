b6888769ff6ee5d2841c7b6138d13e21a5f0b72a
Kevin McBride <kevin@mcbrideux.com>
2023-04-19 14:01:12 -0700

    changed model to gpt-3.5-turbo

--- server/server.js
+++ server/server.js
@@ -27,18 +27,27 @@ app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
+        //const response = await openai.createCompletion({
+        //    model: "text-davinci-003",
+        //     prompt: `${prompt}`,
+        //     temperature: 0,
+        //     max_tokens: 3000,
+        //     top_p: 1,
+        //     frequency_penalty: 0.5,
+        //     presence_penalty: 0,
+        // });
+
+        // res.status(200).send({
+        //     bot: response.data.choices[0].text
+        // });
+
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: `${prompt}`}],
         });
 
         res.status(200).send({
-            bot: response.data.choices[0].text
+            bot: response.data.choices[0].message.content
         });
 
     } catch (error){
