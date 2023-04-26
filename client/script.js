cfb00b2cecb619537da8bdfec37ee836b7579da6
Kevin McBride <kevin@mcbrideux.com>
2023-04-26 16:29:01 -0700

    debuggin openai api call error again

--- client/script.js
+++ client/script.js
@@ -150,8 +150,8 @@ const handleSubmit = async (e) => {
   console.log(promptMessages);
 
   // fetch data from server
-  //const response = await fetch('https://codeguru-q6df.onrender.com', {
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codeguru-q6df.onrender.com', {
+  //const response = await fetch('http://localhost:5000', {
     method: 'POST', 
     headers: {
       'Content-Type' : 'application/json'
